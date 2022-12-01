3d5b2a6a0fe7582a41f4b1da04d95e87fe203538
Gabrielle3101 <95686778+Gabrielle3101@users.noreply.github.com>
2022-12-01 11:00:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+
+const change = document.getElementById("image1");
+
+change.addEventListener("click", (event) => {
+  event.target.style.background = "red"
+  event.target.style.color = "white"
+});
+
+const change2 = document.getElementById("image2");
+
+change.addEventListener("click", (event) => {
+  event.target.style.background = "green"
+  event.target.style.color = "white"
+});
+  
+
